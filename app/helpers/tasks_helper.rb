67f3c00f651a23a8d664065ea7b35ac4add251c4
Alex Mekhovov <alex.mehovov@gmail.com>
2012-01-29 18:48:50 +0200

    modify: IU

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,15 @@
 module TasksHelper
+  
+  def get_todo
+  	tasks = []
+    Project.find_all_by_owner_id(current_user.id).each do |proj| 
+	    proj.stories.each do |story| 
+	      story.tasks.each do |task| 
+		    tasks << task if task.status == :toDo
+		  end
+		end
+	end
+	tasks
+  end
+
 end
